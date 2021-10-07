846b0041437785571c9138e4bf7767b36e7d1464
superzof <60633056+superzof@users.noreply.github.com>
2021-10-06 21:40:39 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace hello_world_cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("entre le nom du compte : ");
+
+        string userName = Console.ReadLine();
+        lgi :
+        {
+             Console.WriteLine("mot de pasee : ");
+        
+            string mdp = Console.ReadLine();
+        }
+
+        Random rd = new Random();
+
+        int rand_num = rd.Next(1,5);
+        {
+            if (rand_num > 1)
+            {
+                goto lgi;
+            }
+            else{
+             Console.WriteLine("sucessfully logged into the accont ");
+            }
+        }
+
+        }
+    }
+}
